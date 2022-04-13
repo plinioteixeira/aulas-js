c6bae28d45f6298bda7257a1a1e13edfd890267b
plinioteixeira <plinio.t.vasconcellos@gmail.com>
2022-04-13 14:12:20 -0300

    Funções

--- js/06-funcoes.js
+++ js/06-funcoes.js
@@ -23,13 +23,22 @@ linha();
 console.log(`Ano: ${ano}`);
 
 
-                //parâmetros/argumentos
-function soma( valor1, valor2 ){
+   //versão 1:             //parâmetros/argumentos
+/* function soma( valor1, valor2 ){
     let total = valor1 + valor2;
     return total;
-}
+} */
 
-console.log( soma(10, 5) );
+// versão 2: sintaxe ARROW FUNTCION (SETA/FLECHA)
+const soma = (valor1, valor2) => {
+    let total = valor1 + valor2;
+    return total;
+};
+
+// Arrow functcion com retorno IMPLÍCITO
+const soma1 = (valor1, valor2) => valor1 + valor2;
+
+console.log( soma1(10, 5) );
 // console.log (total); //erro, pois é variável local
 
 let resultado = soma(100, 50)
@@ -38,3 +47,38 @@ console.log(resultado);
 if(resultado < 1000){
     console.log("Abaixo da meta");
 }
+
+linha();
+
+/* function dobra(numero){
+    let resultado = numero * 2;
+    return resultado;
+}
+ */
+
+/* function dobra(numero){
+    return numero * 2
+} */
+
+
+//VERSÃO 2: arrow function
+/* const dobra = (numero) => {
+    let resultado = numero * 2;
+    return resultado
+};
+ */
+
+/* const dobra = (numero) => {
+    return numero * 2
+}
+ */
+
+// retorno IMPLÍCITO (remove {} e o return)
+// obs.: como só temos um parâmetro (numero), pode tirar os ()
+const dobra = numero => numero * 2;
+
+
+
+
+console.log( dobra (10) );
+console.log( dobra (150) );
